94fdf577020d8e8eb0a8dea8ba8d073eba573d92
Michael Krisper <michael.krisper@gmail.com>
2016-08-02 19:57:03 +0200

    initial

--- template-method/Program.cs
+++ template-method/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace template_method
 {
@@ -10,6 +6,45 @@ namespace template_method
     {
         static void Main(string[] args)
         {
+            new Child1().Operation();
+            new Child2().Operation();
+            new ChildPropertyTemplate().PrintName();
+            Console.ReadKey();
         }
+    }
+
+    abstract class Base
+    {
+        public void Operation()
+        {
+            Action1();
+            Action2();
+        }
+
+        protected abstract void Action1();
+        protected abstract void Action2();
+    }
+
+    class Child1 : Base
+    {
+        protected override void Action1() => Console.WriteLine("Child1.Action1");
+        protected override void Action2() => Console.WriteLine("Child1.Action2");
+    }
+
+    internal class Child2 : Base
+    {
+        protected override void Action1() => Console.WriteLine("Child2.Action1");
+        protected override void Action2() => Console.WriteLine("Child2.Action2");
+    }
+    
+    internal abstract class BasicPropertyTemplate
+    {
+        protected abstract string Name { get;}
+        public void PrintName() => Console.WriteLine($"Parent called {Name}");
+    }
+    
+    internal class ChildPropertyTemplate : BasicPropertyTemplate
+    {
+        protected override string Name => "ChildProperty";
     }
 }
